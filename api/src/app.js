1b69669ab898c7d9573379a3700a473f056a4032
zimbabalim <centrepede@gmail.com>
2021-02-19 22:14:44 +0000

    api service running with nodemon

--- api/src/app.js
+++ api/src/app.js
@@ -4,5 +4,7 @@ const PORT = 3001;
 const app = express();
 
 app.listen(PORT, () => {
-  console.log('/app/ -', PORT);
+  console.log('\n*****************************');
+  console.log('/app/ -RUNNING:', PORT);
+  console.log('*****************************\n');
 });
