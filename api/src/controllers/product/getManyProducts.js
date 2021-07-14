da6db6e4bc936b27d4d0d8a75558aa4cdfed36f4
zimbabalim <centrepede@gmail.com>
2021-07-14 08:06:31 +0100

    discovered that i can call subdocuments already

--- api/src/controllers/product/getManyProducts.js
+++ api/src/controllers/product/getManyProducts.js
@@ -6,45 +6,9 @@ import methods from '../utils/methods.js';
 
 export const getManyProducts = async (req, res) => {
   
-  console.log('/getManyProducts/ -getManyProducts', req.query.friendly_name);
+  console.log('/getManyProducts/ -getManyProducts', req.query);
   
-  // let payload = {};
-  
-  const isQueryForMain = Object.keys(req.query).filter((item) => {
-    return item === 'friendly_name';
-  });
-  
-  
-/*  const queryMain = async () => {
-  
-    let query = {};
-    let payload = {};
-    
-    console.log('/getManyProducts/ -queryMain');
-    query = methods.find({
-      model: Product,
-      filters: req.query,
-    });
-    
-    payload = await finder(req, res, {
-      query
-    });
-    
-    const {data, success} = payload;
-    
-    res.send({
-      success,
-      data
-    });
-  }*/
-  
-  if (isQueryForMain.length === 0) {
-    await queryMain();
-  }
-  
-  
-  // *** this works
-  /* const query = methods.find({
+   const query = methods.find({
      model: Product,
      filters: req.query,
    });
@@ -58,19 +22,7 @@ export const getManyProducts = async (req, res) => {
    res.send({
      success,
      data
-   });*/
-  
-  
-  // parse query (as per users?)
-  // query is for design or pattern (main or variation)?
-  // ---
-  // find design that includes variations(s) that match query
-  
-  // friendly_name finds main!
-  //
-  
-  
-  
+   });
 }
 
 export default getManyProducts;
--- api/src/routes/productRoutes.js
+++ api/src/routes/productRoutes.js
@@ -5,6 +5,8 @@ import createProduct from '../controllers/product/createProduct.js';
 const router = express.Router();
 
 router.get('/product', getManyProducts); // *** get all
+
+
 // router.get('/product/:id', getProduct); // *** get one by id
 router.post('/product', createProduct); // *** create a new one
 // router.delete('/product/:id', deleteProduct); // *** delete one
