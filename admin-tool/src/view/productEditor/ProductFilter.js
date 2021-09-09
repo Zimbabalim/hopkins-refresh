67d0370083f1c74cb3bd5bcbbab13fcb51477973
zimbabalim <centrepede@gmail.com>
2021-09-09 23:11:27 +0100

    added product filter

--- admin-tool/src/view/productEditor/ProductFilter.js
+++ admin-tool/src/view/productEditor/ProductFilter.js
@@ -1,31 +1,47 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 import {actions} from '../../state';
 
 
 const ProductFilter = (props) => {
   
-  useEffect(() => {
-    console.log('/ProductFilter/ -???', actions);
-  },[]);
+  const [tag, setTag] = useState('');
+  const [design, setDesign] = useState('');
   
-  const onSubmit = (payload) => {
-    console.log('/ProductFilter/ -onSubmit====', payload, props, '>>>', actions);
-    // props.dispatch({type: actions.testAction});
-    // props.dispatch(actions.testAction({label: 'boo'}));
-    props.dispatch(actions.getProductData(
-        {path: '/api/product/?partialmatch=true&variations.tags=Organza'}
-    ));
-    //getProductData
+  const onSubmitIntention = (key, type) => {
+    if (key !== 'Enter') return;
+    
+    if (type === 'design') {
+      setTag('');
+      props.dispatch(actions.getProductData(
+          {path: `/api/product/?partialmatch=true&friendly_name=${design}`}
+      ));
+    }
+    
+    if (type === 'tag') {
+      setDesign('');
+      props.dispatch(actions.getProductData(
+          {path: `/api/product/?partialmatch=true&variations.tags=${tag}`}
+      ));
+    }
   }
   
   return (
       <>
         <h4>ProductFilter</h4>
-        <button onClick={() => {
-          onSubmit('foo');
-        }}>SUBMIT</button>
-        <p>??? {props.testFlag.label}</p>
+        
+        <input type="text" placeholder="design" value={design}
+               onChange={ e => setDesign(e.target.value) }
+               onKeyPress={(e) => {
+                 onSubmitIntention(e.key, 'design');
+               }}/>
+        
+        <input type="text" placeholder="tag" value={tag}
+               onChange={ e => setTag(e.target.value) }
+               onKeyPress={(e) => {
+                 onSubmitIntention(e.key, 'tag');
+               }}
+        />
       </>
   );
 };
--- api/src/controllers/utils/methods.js
+++ api/src/controllers/utils/methods.js
@@ -21,6 +21,9 @@ const methods = {
     const params = methods.castNumericValues(options.filters);
     const query = options.model.find({});
     const partialmatch = (params.partialmatch === 'true');
+    const all = (params.all === 'true');
+    
+    console.log('/methods/ -find', all);
     
     query.limit(params.limit || 0);
     query.skip(params.skip || 0);
@@ -29,14 +32,51 @@ const methods = {
     delete params.limit;
     delete params.skip;
     delete params.partialmatch;
+    delete params.all;
     
     Object.keys(params).map((key) => {
       let value = params[key];
-      if (partialmatch) {
+      
+      /*if (partialmatch) { // *** WORKS
         query.find({[key]: {"$regex": value, "$options": "i" }});
       } else {
         query.find({[key]: value});
+      }*/
+  
+      if (partialmatch) { // *** WORKS
+        console.log('/methods/ -FIND');
+        query.find({[key]: {"$regex": value, "$options": "i" }});
+        return;
       }
+      console.log('/methods/ -FALL THRU?');
+  
+      // *** TODO accept multiple tags, doesn't seem to be working
+      if (all) {
+        const valueToArray = value.split(',');
+        console.log('/methods/ -ALL =================', valueToArray);
+        query.find({[key]: {"$all": valueToArray }});
+        
+        //find().all(function(arr)
+        /*query.find().all((z) => {
+          console.log('/methods/ -', z);
+        })*/
+        return;
+      }
+  
+      query.find({[key]: value});
+  
+      // *** TODO refactor - switch statement?
+      /*if (all) {
+        
+        console.log('/methods/ -ALL =================');
+        
+        const valueToArray = value.split(',');
+        console.log('/methods/ -valueToArray', valueToArray);
+        
+        query.find({[key]: {"$all": valueToArray, "$options": "i" }});
+      } else {
+        query.find({[key]: value});
+      }*/
     })
     
     return query;
