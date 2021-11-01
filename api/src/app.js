0a0c22a1b9d60a65f520b520ae1b157eec96ee06
zimbabalim <centrepede@gmail.com>
2021-11-01 22:40:11 +0000

    authd mongo

--- api/src/app.js
+++ api/src/app.js
@@ -6,7 +6,11 @@ import * as path from 'path';
 const DB_PORT = process.env.DB_PORT || 27017;
 const DB_URL = process.env.DB_URL;
 const DB_NAME = process.env.DB_NAME;
-const dbPath = `mongodb://${DB_URL}:${DB_PORT}/${DB_NAME}`;
+const auth = 'maestro:maestro@';
+
+// const dbPath = `mongodb://${DB_URL}:${DB_PORT}/${DB_NAME}`;
+const dbPath = `mongodb://${auth}${DB_URL}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+
 mongoose.connect(dbPath, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 mongoose.set('debug', true);
@@ -20,7 +24,7 @@ initRoutes(app);
 
 app.listen(PORT, () => {
   console.log('\n*****************************');
-  console.log('/app/ -RUNNING:', PORT);
+  console.log('/app/ -RUNNING:', PORT, 'db:', DB_PORT, 'connection:', dbPath);
   console.log('*****************************\n');
 });
 
