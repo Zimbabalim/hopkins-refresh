5a832e63058b0a96025be50e2de3329bc4a7ff98
zimbabalim <centrepede@gmail.com>
2022-10-06 14:18:21 +0100

    createProduct also writes to config_designs

--- api/src/controllers/product/createProduct.js
+++ api/src/controllers/product/createProduct.js
@@ -1,4 +1,5 @@
 import Product from '../../models/Product.model.js';
+import Design from '../../models/configFragments/Design.model.js';
 
 export const createProduct = async (req, res) => {
   console.log('\n*** /PRODUCT.controller/ -createProduct\n', req.body);
@@ -11,6 +12,12 @@ export const createProduct = async (req, res) => {
           success: false
         });
   }
+  
+  const cleanStrings = {
+    code: req.body.code.replace(/\//g, ''),
+    uid: req.body.friendly_name.replace(/ /g, '_'), // *** for config design (spaces with underscore)
+  }
+  // console.log('/createProduct/ -createProduct', cleanStrings);
   
   
   // *** ugly but works
@@ -39,7 +46,7 @@ export const createProduct = async (req, res) => {
   
   const product = new Product({
     friendly_name: req.body.friendly_name,
-    default_product_code: req.body.code,
+    default_product_code: req.body.code, // FIXIT is this correct at the point of creation?
     variations: [],
   });
   
@@ -70,7 +77,23 @@ export const createProduct = async (req, res) => {
         },
         success: true,
         data
-      })
+      });
+  
+  // *** 5.10.22
+  // *** also add entry to 'config_designs' collection - this is the lookup that will allow the entry to appear on the .com site
+  // TODO seems to work, but there is no visibility for failure, this class has become a dirty composite
+  const configDesign = new Design({
+    code: cleanStrings.code,
+    label: req.body.friendly_name,
+    uid: cleanStrings.uid,
+  });
+  
+  try {
+    await configDesign.save();
+  } catch (error) {
+    console.error('/USER.controller/ -createProduct [configDesign] --ERROR', error);
+  }
+  console.log('/createProduct/ -createProduct [configDesign] --SUCCESS');
 }
 
 export default createProduct;
